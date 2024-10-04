84fc796455b6b655804f67bb399c21c8ca5406ed
suryawanshiS <surajdtis2002@gmail.com>
2024-10-04 13:29:22 +0530

    updated code

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,18 +1,38 @@
+// import React, { createContext } from "react";
+// import all_product from "../Components/Assets/all_product"
+
+
+// export const ShopContext = createContext(null);
+
+// const ShopContextProvider = (props) => {
+
+//     const contextValue = {all_product};
+
+//     return (
+//         <ShopContext.Provider value={contextValue}>
+//             {props.children}
+//         </ShopContext.Provider>
+//     )
+
+// }
+// export default ShopContextProvider;
+
 import React, { createContext } from "react";
-import all_product from "../Components/Assets/all_product"
+import all_product from "../Components/Assets/all_product";
+
 
 
 export const ShopContext = createContext(null);
 
 const ShopContextProvider = (props) => {
 
-    const contextValue = {all_product};
+    const contextValue = {all_product}
 
     return (
         <ShopContext.Provider value={contextValue}>
             {props.children}
         </ShopContext.Provider>
     )
-
 }
+
 export default ShopContextProvider;
