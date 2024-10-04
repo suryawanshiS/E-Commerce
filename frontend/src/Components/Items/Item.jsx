2faf007505bbe4a667524112287b9e091dac1059
suryawanshiS <surajdtis2002@gmail.com>
2024-10-04 14:30:40 +0530

    next code

--- frontend/src/Components/Items/Item.jsx
+++ frontend/src/Components/Items/Item.jsx
@@ -1,23 +1,50 @@
-import React from 'react'
-import './Item.css'
+// import React from 'react'
+// import './Item.css'
+
+// const Item = (props) => {
+//   return (
+//     <div className='item'>
+//         <link to={`/product/${props.id}`}><img src={props.image} alt="" /></link>
+//         <p>{props.name}</p>
+//         <div className="item-prices">
+//             <div className="item-price-new">
+//                 ${props.new_price}
+                
+//             </div>
+//             <div className="item-price-old">
+//                 ${props.old_price}
+
+//             </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Item
+
+
+import React from 'react';
+import { Link } from 'react-router-dom';  // Import Link
+import './Item.css';
 
 const Item = (props) => {
   return (
     <div className='item'>
-        <img src={props.image} alt="" />
-        <p>{props.name}</p>
-        <div className="item-prices">
-            <div className="item-price-new">
-                ${props.new_price}
-                
-            </div>
-            <div className="item-price-old">
-                ${props.old_price}
-
-            </div>
+      {/* Use Link component for routing */}
+      <Link to={`/product/${props.id}`}>
+        <img src={props.image} alt={props.name} />
+      </Link>
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">
+          ${props.new_price}
         </div>
+        <div className="item-price-old">
+          ${props.old_price}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Item
+export default Item;
