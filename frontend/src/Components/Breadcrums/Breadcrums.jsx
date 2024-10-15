18bfbb44860e2e62ec0654f5c004a271fa645479
suryawanshiS <surajdtis2002@gmail.com>
2024-10-15 18:44:57 +0530

    make some changes in css files and add some new code

--- frontend/src/Components/Breadcrums/Breadcrums.jsx
+++ frontend/src/Components/Breadcrums/Breadcrums.jsx
@@ -3,10 +3,10 @@ import './Breadcrums.css'
 import arrow_icon from '../Assets/breadcrum_arrow.png'
 
 const Breadcrums = (props) => {
-    const {product} = props;
+  const { product } = props;
   return (
     <div className='breadcrums'>
-        HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
+      HOME <img src={arrow_icon} alt="" /> SHOP <img src={arrow_icon} alt="" /> {product.category} <img src={arrow_icon} alt="" /> {product.name}
 
     </div>
   )
